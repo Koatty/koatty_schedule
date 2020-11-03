7b649a00df1817374b0b737e30dd6ab512742e85
linyyyang <linyyyang@tencent.com>
2020-11-03 09:30:21 +0800

    remove dist

--- dist/lib.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * get metadata value of a metadata key on the prototype chain of an object and property
- * @param metadataKey metadata's key
- * @param target the target of metadataKey
- */
-export declare function recursiveGetMetadata(metadataKey: any, target: any, propertyKey?: string | symbol): any[];
